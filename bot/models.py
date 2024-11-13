1c528ec28f6f2b99b2d0f978c8e8bea2155efab3
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2024-11-13 16:26:34 +0500

    modles commit delete

--- bot/models.py
+++ bot/models.py
@@ -1,7 +1,5 @@
-from django.contrib.auth.models import AbstractUser, UserManager, Group, Permission
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.hashers import make_password
 from bot.validators import phone_number_validator
 
 
